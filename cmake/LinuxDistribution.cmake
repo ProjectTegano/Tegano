9c11bc15c202b82cc9ff0d3c9228a19172de46df
Andreas Baumann <abaumann@yahoo.com>
2014-10-21 18:47:55 +0200

    fixed linking and detection issues for PostgreSQL on Ubutnu 12.04

---
+++ cmake/LinuxDistribution.cmake
@@ -0,0 +1,30 @@
+# from http://thomasfischer.biz/find-out-the-linux-distribution-and-version-in-cmake/
+
+# use the LSB stuff if possible :)
+EXECUTE_PROCESS(
+  COMMAND cat /etc/lsb-release
+  COMMAND grep DISTRIB_ID
+  COMMAND awk -F= "{ print $2 }"
+  COMMAND tr "\n" " "
+  COMMAND sed "s/ //"
+  OUTPUT_VARIABLE LSB_ID
+  RESULT_VARIABLE LSB_ID_RESULT
+)
+EXECUTE_PROCESS(
+  COMMAND cat /etc/lsb-release
+  COMMAND grep DISTRIB_RELEASE
+  COMMAND awk -F= "{ print $2 }"
+  COMMAND tr "\n" " "
+  COMMAND sed "s/ //"
+  OUTPUT_VARIABLE LSB_VER
+  RESULT_VARIABLE LSB_VER_RESULT
+)
+
+message("LSB output: ${LSB_ID_RESULT}:${LSB_ID} ${LSB_VER_RESULT}:${LSB_VER}")
+
+if(NOT ${LSB_ID} STREQUAL "")
+  # found some, use it :D
+  set(INSTALLER_PLATFORM "${LSB_ID}-${LSB_VER}" CACHE PATH "Installer chosen platform")
+else(NOT ${LSB_ID} STREQUAL "")
+  set(INSTALLER_PLATFORM "linux-generic" CACHE PATH "Installer chosen platform")
+endif(NOT ${LSB_ID} STREQUAL "")
