13c591315aab0f402384af645ab74ca0710c2102
Andreas Baumann <mail@andreasbaumann.cc>
2014-10-22 13:43:38 +0200

    removed old makefile system

--- makefiles/nmake/dochelp.mk
+++
@@ -1,6 +0,0 @@
-
-Available targets:
-
-make doc                    create all documentation (except man pages and epub)
-make doc-doxygen            build CHM version of doxygen documentation
-make doc-htmlhelp           build CHM version of the Wolframe documentation
