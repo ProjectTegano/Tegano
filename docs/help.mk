21f662de3a1ce64d75dc518e36ded3bd8474f4a4
Andreas Baumann <mail@andreasbaumann.cc>
2014-10-22 13:42:57 +0200

    reactivated 'make doc' (on Linux only) with old standalone makefiles

---
+++ docs/help.mk
@@ -0,0 +1,21 @@
+
+Available documentation targets:
+
+doc                      create all documentation (except man pages and epub)
+man                      create man pages
+doc-doxygen              build HTML and man version of doxygen documentation
+doc-doxygen-webpage      build HTML and man version of doxygen documentation (for webpage)
+doc-html                 build HTML version of the manual and tutorial
+                         (one big html only)
+doc-html-chunked         build chunked HTML version of the manual
+doc-html-webpage         build one-file HTML version of the manual and
+                         tutorial (for webpage)
+doc-html-webpage-chunked build chunked HTML version of the manual and
+                         tutorial (for webpage)
+doc-pdf                  build PDF version of the manual and tutorial
+doc-epub                 build EPUB version of the manual and tutorial
+doc-towordml             convert docbook to WordML
+doc-txt                  build plain text version
+
+check                    test the validity of the documenation source and tools
+
